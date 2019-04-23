7e49ba561802c2e94bcf2507f519c2ea6c89d66a
Lei Hua <lhua@example.com>
2019-04-22 21:05:21 -0400

    two navbars

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,7 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path, include
 
@@ -5,3 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
