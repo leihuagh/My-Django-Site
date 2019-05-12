97303e22917842cf7d1c52622c3e46361e2e9e15
Lei Hua <lhua@example.com>
2019-05-11 21:34:22 -0400

    login

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path    
-from home.views import index 
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from home.views import index
 
 urlpatterns = [
     path('', index, name='index'),
+    path('login', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout', LogoutView.as_view(template_name='logout.html'), name='logout')
 ]
